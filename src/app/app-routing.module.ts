7c467a06cd45091034d12eddd7a3d98815318024
ralarconb <ralarconbazan@gmail.com>
2022-04-14 09:47:54 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+// routing check #3, import router module and routes
 import { RouterModule, Routes } from '@angular/router';
+// routing check #4, import routed components
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 
+// routing check #5, define routes array
 const routes: Routes = [
   { path: 'first-component', component: FirstComponent },
   { path: 'second-component', component: SecondComponent },
 ];
 
+// routing check #6, configure ng module imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// routing check #7, import app routing module
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
@@ -8,6 +8,7 @@ import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [AppComponent, FirstComponent, SecondComponent],
+  /* routing check #8, add app routing module to imports array */
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
